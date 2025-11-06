a22af8762081b68d233bda68f9f94644e5c1b422
marieleponti <mariele.ponticiello@temple.edu>
2025-11-06 16:32:18 -0500

    remove Donation, Support Us, Press

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,18 +26,18 @@
   <title>RESISTING BORDER TECH</title>
   <meta
     name="description"
-    content="Queering the Map is a community generated counter-mapping platform for digitally archiving LGBTQ2IA+ experience in relation to physical space."
+    content=""
   />
 
   <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://www.queeringthemap.com/" />
-  <meta property="og:title" content="Queering The Map" />
+  <meta property="og:url" content="" />
+  <meta property="og:title" content="" />
   <meta property="og:image" content={qtm_sharing_image} />
   <meta
     property="og:description"
-    content="Queering the Map is a community generated counter-mapping platform for digitally archiving LGBTQ2IA+ experience in relation to physical space."
+    content=""
   />
-  <meta property="og:site_name" content="Queering The Map" />
+  <meta property="og:site_name" content="" />
   <meta property="og:locale" content="en_US" />
   <meta
     name="google-site-verification"
@@ -45,10 +45,10 @@
   />
 
   <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:title" content="Queering The Map" />
+  <meta name="twitter:title" content="" />
   <meta
     name="twitter:description"
-    content="Queering the Map is a community generated counter-mapping platform for digitally archiving LGBTQ2IA+ experience in relation to physical space."
+    content=""
   />
   <meta name="twitter:image" content={qtm_sharing_image} />
 </svelte:head>
