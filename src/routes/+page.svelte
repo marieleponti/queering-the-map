4616002828e0aaeb583d0e0c45bde67ed6b2d5fa
marieleponti <mariele.ponticiello@temple.edu>
2025-11-07 11:48:10 -0500

    revert InfoOverlay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,14 +23,20 @@
 
 <svelte:head>
   <title>RESISTING BORDER TECH</title>
-  <meta name="description" content="" />
+  <meta
+    name="description"
+    content="Queering the Map is a community generated counter-mapping platform for digitally archiving LGBTQ2IA+ experience in relation to physical space."
+  />
 
   <meta property="og:type" content="website" />
-  <meta property="og:url" content="" />
-  <meta property="og:title" content="" />
+  <meta property="og:url" content="https://www.queeringthemap.com/" />
+  <meta property="og:title" content="Queering The Map" />
   <meta property="og:image" content={qtm_sharing_image} />
-  <meta property="og:description" content="" />
-  <meta property="og:site_name" content="" />
+  <meta
+    property="og:description"
+    content="Queering the Map is a community generated counter-mapping platform for digitally archiving LGBTQ2IA+ experience in relation to physical space."
+  />
+  <meta property="og:site_name" content="Queering The Map" />
   <meta property="og:locale" content="en_US" />
   <meta
     name="google-site-verification"
@@ -38,8 +44,11 @@
   />
 
   <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:title" content="" />
-  <meta name="twitter:description" content="" />
+  <meta name="twitter:title" content="Queering The Map" />
+  <meta
+    name="twitter:description"
+    content="Queering the Map is a community generated counter-mapping platform for digitally archiving LGBTQ2IA+ experience in relation to physical space."
+  />
   <meta name="twitter:image" content={qtm_sharing_image} />
 </svelte:head>
 
